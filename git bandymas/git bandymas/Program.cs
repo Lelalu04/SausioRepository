3a023e10ebe22a5b587eda4fcbf737fabdf2f1de
Lelalu04 <ricrdascep@gmail.com>
2022-01-10 21:09:02 +0200

    keliu i main

---
+++ git bandymas/git bandymas/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_bandymas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string limonadas = "limonadas";
+            string arbata = "Arbata";
+            string kakava = "Kakava";
+            string kava = "Kava";
+            string nieko = "Nieko";
+
+            Console.WriteLine("1 - Limonadas");
+            Console.WriteLine("2 - Arbata");
+            Console.WriteLine("3 - Kakava");
+            Console.WriteLine("4 - Kava");
+            Console.WriteLine("5 - Nieko");
+
+
+
+            Console.WriteLine("pasirinkite norima punkta:");
+            int pasirinkimas = int.Parse(Console.ReadLine());
+
+            switch (pasirinkimas)
+            {
+                case 1:
+                    Console.WriteLine($"Jus pasirinkote: {limonadas}");
+                    break;
+                case 2:
+                    Console.WriteLine($"Jus pasirinkote: {arbata}");
+                    break;
+                case 3:
+                    Console.WriteLine($"Jus pasirinkote: {kakava}");
+                    break;
+                case 4:
+                    Console.WriteLine($"Jus pasirinkote: {kava}");
+                    break;
+                case 5:
+                    Console.WriteLine($"Jus pasirinkote: {nieko}");
+                    break;
+                default:
+                    Console.WriteLine("blogai ivestas pasirinkimas");
+                    break;
+
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
